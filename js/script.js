1d9a432009b14dd06a551c9079d83122d6ad8286
darya <dariy060100@mail.ru>
2020-08-10 14:44:49 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -1,37 +1,50 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
-    let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++){
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-        }
+    // Timer
+    let deadline = '2020-09-01'
+
+    function setTimeRemaining(endTime){
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000)%60),
+            minutes = Math.floor((t/1000/60)%60),
+            hours = Math.floor((t/1000/60/60)%60);
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        } ;
     }
 
-    hideTabContent(1);
+    function setClock(endTime) {
+        let hours = document.querySelector('.hours'),
+            minutes = document.querySelector('.minutes'),
+            seconds = document.querySelector('.seconds');
+        if ((Date.parse(endTime) - Date.parse(new Date())) <= 0){
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
+        } else {
+            let timeInterval = setInterval(updateClock, 1000);
 
-    function showTabContent(b) {
-        if (tabContent[b].classList.contains('hide')){
-            tabContent[b].classList.remove('hide');
-            tabContent[b].classList.add('show');
-        }
-    }
+            function updateClock() {
+                let time = setTimeRemaining(endTime);
+                function addZero(num) {
+                    if (num <= 9 ){
+                        return '0' + num;
+                    } else return num;
+                }
 
-    info.addEventListener('click', function (event) {
-        let target = event.target;
-        if (target && target.classList.contains('info-header-tab')){
-            for (let i = 0; i < tab.length; i++){
-                if (target == tab[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
-                    break;
+                hours.textContent = addZero(time.hours);
+                minutes.textContent = addZero(time.minutes);
+                seconds.textContent = addZero(time.seconds);
+                if (time.total <= 0) {
+                    clearInterval(timeInterval);
                 }
             }
         }
-    })
-    
-
+    }
+    setClock(deadline);
 });
